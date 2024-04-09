bfce2f1fd7ba165a6208c3d4d5d113fc7424765a
devinrenca <166529953+devinrenca@users.noreply.github.com>
2024-04-09 11:01:33 -0400

    Add files via upload

---
+++ script/loadfade.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {	
+	  $('body').fadeIn(900);
+});
+
+$(document).ready(function(){
+  $("#credits_link").click(function(){
+    $("#reel_credits").toggle(150);
+  });
+});
